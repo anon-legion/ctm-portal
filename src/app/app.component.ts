10313cbdc0af201c113330176c43cad54a7cc8cb
=GV= <g.sync.mobile1@gmail.com>
2023-08-15 17:46:52 +0800

    add mat-tab-nav-bar ui component for app navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd, Event as RouterEvent } from '@angular/router';
 
+interface Link {
+  label: string;
+  path: string;
+  queryParams: { [key: string]: string };
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor() {
-    // do nothing
+  links: Link[] = [
+    { label: 'Cities', path: 'cities', queryParams: {} },
+    { label: 'Routes', path: 'bus-routes', queryParams: {} },
+    { label: 'Places', path: 'places', queryParams: {} },
+    {
+      label: 'Route Stops',
+      path: 'route-stops',
+      queryParams: { cityId: 'all', routeId: 'all' },
+    },
+  ];
+  currentRoute = '';
+  constructor(private _router: Router) {
+    this._router.events.subscribe((e: RouterEvent) => {
+      if (e instanceof NavigationEnd) {
+        this.currentRoute = e.url.split('?')[0].replace('/', '');
+        console.log(this.currentRoute);
+      }
+    });
+  }
+
+  anchorOnClick(link: Link) {
+    this._router.navigate([link.path], {
+      queryParams: link.queryParams,
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,7 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    // FormsModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
