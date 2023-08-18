30c3b95432babe240469d2bd6db04915fb774918
=GV= <g.sync.mobile1@gmail.com>
2023-08-18 16:01:21 +0800

    add tentative get-all-data shared helper founction for use of place and bus-route component

---
+++ src/app/shared/get-all-data.ts
@@ -0,0 +1,30 @@
+import { HttpStatusCode as StatusCode } from '@angular/common/http';
+import { DataService } from '../data.service';
+import TableDataSource from '../shared/table-data-source';
+import { PlaceTd, BusRoute } from '../types';
+
+function getAllData<T extends PlaceTd | BusRoute>(
+  service: DataService,
+  placeList: TableDataSource<T>,
+  type: 'place' | 'busRoute'
+) {
+  if (type === 'place') {
+    service.getAllPlaces().then(res => {
+      if (res.status !== StatusCode.Ok || !res.data.length) {
+        placeList.setData([]);
+        return;
+      }
+      placeList.setData(res.data as T[]);
+    });
+  }
+
+  if (type === 'busRoute') {
+    service.getAllBusRoutes().then(res => {
+      if (res.status !== StatusCode.Ok || !res.data.length) {
+        placeList.setData([]);
+        return;
+      }
+      placeList.setData(res.data as T[]);
+    });
+  }
+}
